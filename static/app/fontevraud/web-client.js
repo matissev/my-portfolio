de20eebb41dbdaf6fec6157038336442860bdfa0
Matisse Vrignaud <matisse@mbp-matisse.local>
2024-02-10 16:12:22 +0100

    Done sockets

---
+++ static/app/fontevraud/web-client.js
@@ -0,0 +1,35 @@
+// add CDN for socket.io and link to this file in index.html header
+
+
+// Variables
+const serverURL = "http://localhost:8100";  // make sure you EDIT THIS!
+                                                               // use http://localhost:3000
+                                                               // if running server locally
+// Client Initialization
+const socket = io(serverURL);
+
+// RECEIVE
+
+socket.on("connect", () => {            
+    console.log("Connected to server!");
+});
+
+// SEND
+
+var actions = document.querySelectorAll(".action");
+
+console.log(actions);
+
+for(i=0; i<actions.length; i++){
+	actions[i].addEventListener('click', function() {
+        console.log(this.value);
+        socket.emit("message", this.value);
+	})
+}
+
+// document.querySelectorAll(".action").onclick = (el) => {
+//     console.log(el);
+//     // let textBox = document.querySelector("#inputBox"); // select the input box
+//     // socket.emit("message", textBox.value );            // send typed text
+//     // textBox.value = "";                                // clear the input box
+// };
---
+++ static/static.cjs
@@ -0,0 +1,16 @@
+// Load Express (a node.js framework), socket.io (for a fast data stream), and osc (to send the message to MAX)
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+// This is the port on which the website is available
+server.listen(8004);
+
+// This is just to tell the server to use the folder app/ as the public web folder
+// This way, if your index.html asks for the files scripts.js and style.js, this is where the server will search for them
+app.use(express.static(__dirname + '/app'));
+
+// If the web client asks for the root folder, deliver him index.html
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/app/index.html');
+});
